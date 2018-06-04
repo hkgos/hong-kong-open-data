2b1135268356950337a8dc58bea05e5976c91df4
Raven Hon <hon@codeastar.com>
2018-06-04 13:49:20 +0800

    Update hkod_en.py

--- hkod/spiders/hkod_en.py
+++ hkod/spiders/hkod_en.py
@@ -3,11 +3,17 @@ import scrapy
 from hkod.items import HkodItem
 import datetime
 
-def getDataInfo():
+def getDataInfo(data):
     hkodItem = HkodItem()
     
+    hkodItem['category'] = data['category']
+    hkodItem['name'] = data['name']
+    hkodItem['org'] = data['org']
+    hkodItem['data_format'] = data['data_format']
+    hkodItem['update_freq'] = data['update_freq']
+    hkodItem['desc'] = data['desc']
+    hkodItem['api'] = data['api']
     
-
     return hkodItem
 
 
@@ -23,13 +29,13 @@ class HkodEnSpider(scrapy.Spider):
             link = cat.css("a::attr(href)").extract_first()
             name = cat.css("a::attr(title)").extract_first()
             link = response.urljoin(link)
-            self.log(link)
-            self.log(name)
-            if (name == "Development" or name == "Food"):
-                request = scrapy.Request(link, callback=self.parse_cat_listing)
-                request.meta['cat_link'] = link
-                request.meta['category'] = name
-                yield request
+#            self.log(link)
+#            self.log(name)
+#            if (name == "Development" or name == "Food"):
+            request = scrapy.Request(link, callback=self.parse_cat_listing)
+            request.meta['cat_link'] = link
+            request.meta['category'] = name
+            yield request
         
     def parse_cat_listing(self, response):
         cat_link = response.meta['cat_link']
@@ -39,18 +45,33 @@ class HkodEnSpider(scrapy.Spider):
         self.log("Dataset size: {}".format(len(datasets)))
         
         for dataset in datasets:
+            data = {}
             name = dataset.css(".dataset-heading a::text").extract_first()
             link = dataset.css(".dataset-heading a::attr(href)").extract_first()
             link = response.urljoin(link)
-            self.log(" Name: {}".format(name))
+#            self.log(" Name: {}".format(name))
             org =  dataset.css(".organization a::text").extract_first()
-            self.log(" Organization: {}".format(org))
+#            self.log(" Organization: {}".format(org))
             formats = dataset.css(".dataset-resources.list-unstyled li a::text").extract()
             data_format = "/".join(formats)
-#            data_format = '/'.join(map(str, formats)) 
-            self.log(" Format Len: {}".format(len(formats)))
-            self.log(" Formats: " + data_format)
+#            self.log(" Format Len: {}".format(len(formats)))
+#            self.log(" Formats: " + data_format)
+            desc = dataset.css(".notes ::text").extract_first()
+#            self.log(" Desc: " + desc)
+            api_flag = dataset.css(".api-label").extract_first()
+            api_flag = 'Y' if api_flag is not None else 'N'
+#            self.log(" API: " + api_flag)
+
+            data['category'] = category
+            data['name'] = name
+            data['link'] = link 
+            data['org'] = org
+            data['data_format'] = data_format
+            data['desc'] = desc 
+            data['api']  = api_flag
+    
             request = scrapy.Request(link, callback=self.parse_dataset_detail)
+            request.meta['data'] = data
             yield request
 #           getDataInfo(category)
         
@@ -68,5 +89,10 @@ class HkodEnSpider(scrapy.Spider):
             self.log("No more")
            
     def parse_dataset_detail(self, response):     
+        data = response.meta['data']
         update_freq = response.css(".update-frequency-area ::text").extract_first() 
-        self.log(" UD Freq>"+update_freq)
+#        self.log(" Frequency:"+update_freq)
+        data['update_freq']  = update_freq
+       
+        dataItem = getDataInfo(data)
+        yield dataItem    
